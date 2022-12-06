6a01188078b98c65186fc894cd2391d5e2d9c998
metincansiper <metincansiper@gmail.com>
2022-12-05 23:43:09 -0800

    Added github workflow file and a hello world test

---
+++ test/temptest.ts
@@ -0,0 +1,8 @@
+import mocha from 'mocha';
+import { expect } from 'chai';
+
+describe("Hello world", function() {
+    it('1 should be 1', function() {
+        expect(1).to.equal(1);
+    });
+});

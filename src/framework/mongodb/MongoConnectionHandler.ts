f9dc47007067f106cb3a268b8534e96ff1df8bd6
metincansiper <metincansiper@gmail.com>
2022-12-10 23:50:56 -0800

    Configure mongo db connection parameters

--- src/framework/mongodb/MongoConnectionHandler.ts
+++ src/framework/mongodb/MongoConnectionHandler.ts
@@ -1,9 +1,9 @@
 import { connect } from 'mongoose';
+import { MONGO_HOST, MONGO_PORT, MONGO_TABLE } from '../../config';
 
 class MongoConnectionHandler implements DBConnectionHandler {
     async connect(): Promise<void> {
-        // TODO: build url with the parameters from the configuration file
-        const url = 'mongodb://127.0.0.1:27017/shoppingcart';
+        const url = `${MONGO_HOST}:${MONGO_PORT}/${MONGO_TABLE}`;
         await connect(url);
     }
 }

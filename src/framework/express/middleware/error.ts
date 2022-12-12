fe6c8e7d0aa1ee5dc4029b6024b8ef70bf07c2a5
metincansiper <metincansiper@gmail.com>
2022-12-11 18:52:32 -0800

    Fixed the typo in error handling middleware

--- src/framework/express/middleware/error.ts
+++ src/framework/express/middleware/error.ts
@@ -1,7 +1,7 @@
 import type { ErrorRequestHandler } from "express";
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
     const code = err.code || 400;
-    res.status(400).send({ error: err.message });
+    res.status(code).send({ error: err.message });
 };
 
 export default errorHandler;
